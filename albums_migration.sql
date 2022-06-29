977215f9ce158d9937aa348faca27688fff7481e
Chris Merritt <christopher.aa.merritt@gmail.com>
2022-06-29 14:47:57 -0500

    complete exercise for tables

---
+++ albums_migration.sql
@@ -0,0 +1,12 @@
+USE codeup_test_db;
+
+DROP TABLE IF EXISTS albums;
+
+ CREATE TABLE IF NOT EXISTS albums(
+ id INT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
+ artist VARCHAR(50) NOT NULL,
+ name VARCHAR(50) NOT NULL,
+ release_date INT,
+ sales FLOAT,
+ genre CHAR(20)
+ );
