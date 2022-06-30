002a6f9cc6be0705a5a41007e210c89de2b9ef1d
Chris Merritt <christopher.aa.merritt@gmail.com>
2022-06-30 11:37:11 -0500

    create select exercises and complete seeder

---
+++ select_exercises.sql
@@ -0,0 +1,16 @@
+USE codeup_test_db;
+
+SELECT * FROM albums WHERE artist = 'Pink Floyd';
+
+SELECT release_date FROM albums WHERE name = 'Sgt.Peppers Lonely Hearts Club Band';
+
+SELECT genre FROM albums Where name = 'Nevermind';
+
+SELECT * FROM albums WHERE release_date BETWEEN 1990 AND 2000;
+
+SELECT * FROM albums WHERE sales < 20;
+
+SELECT * FROM albums WHERE genre = 'Rock';
+
+
+
