5c90c8e79b4d1980cb2f9f0a3ac27ab475f115d6
Chris Merritt <christopher.aa.merritt@gmail.com>
2022-07-01 14:59:03 -0500

    create: make file for functions and complete it

---
+++ functions_exercises.sql
@@ -0,0 +1,54 @@
+USE employees;
+
+# Select * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+Select *
+FROM employees
+WHERE gender = 'M'
+  AND (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya')
+ORDER BY last_name DESC, first_name DESC;
+
+SELECT *
+FROM employees
+WHERE last_name LIKE '%e%'
+ORDER BY emp_no DESC;
+
+SELECT *
+FROM employees
+WHERE last_name LIKE 'E%'
+   OR last_name LIKE '%e';
+
+SELECT concat(first_name, ' ', last_name) AS full_name
+FROM employees
+WHERE last_name LIKE 'E%e';
+
+SELECT *
+FROM employees
+WHERE last_name like '%q%';
+
+SELECT *
+FROM employees
+WHERE last_name LIKE '%q%'
+  AND last_name NOT LIKE '%qu%';
+
+SELECT *
+FROM employees
+WHERE birth_date LIKE '%-12-25';
+
+SELECT *
+FROM employees
+WHERE birth_date LIKE '%-12-25'
+AND hire_date BETWEEN '1990-01-01' AND '1999-12-31';
+
+SELECT *
+FROM employees
+WHERE birth_date LIKE '%-12-25'
+AND hire_date BETWEEN '1990-01-01' AND '1999-12-31'
+ORDER BY birth_date, hire_date DESC;
+
+SELECT first_name, last_name, datediff(curdate(), hire_date) AS days_worked
+FROM employees
+WHERE birth_date LIKE '%-12-25'
+AND hire_date BETWEEN '1990-01-01' AND '1999-12-31';
+
+
+
